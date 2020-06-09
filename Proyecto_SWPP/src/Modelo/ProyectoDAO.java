12ab4b041c3a829f890c254863545fe417d2826d
Dan Javier <62302748+DanteGalante@users.noreply.github.com>
2020-06-09 09:05:25 -0500

    Update ProyectoDAO.java

--- Proyecto_SWPP/src/Modelo/ProyectoDAO.java
+++ Proyecto_SWPP/src/Modelo/ProyectoDAO.java
@@ -12,7 +12,6 @@ import java.util.List;
 /**
  * Clave del programa: SWPP                                                         
  * Autor: @author olver
- * Descripción: descripción clara de lo que hace el programa
  * Fecha: 09/06/2020
  * Version: 1.0
  * Descripción: 
